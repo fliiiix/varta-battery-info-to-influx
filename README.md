c492c5b688e7e5f34ae34e5f0808c82499614f14
fliiiix <hi@l33t.name>
2021-07-09 10:09:19 +0200

    Version 2.0 parse xml instead of weird js parsing

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Replace this config in `__main__.py` with the correct values.
 
 ```
 BATTERY = {
-    "url": "http://hostname/cgi/ems_data.js",
+    "url": "http://hostname/cgi/ems_data.xml",
     "name": "TheBattery"
 }
 
@@ -15,3 +15,6 @@ INFLUX = {
     "password":"lepassword"
 }
 ```
+
+Parsing state of charge as percentage and state of the battery and post it to influx.
+See [docs]( https://www.loxwiki.eu/pages/viewpage.action?pageId=39354750 ).
--- __main__.py
+++ __main__.py
@@ -1,8 +1,8 @@
 import requests
-import js2py
+import xml.etree.ElementTree as ET
 
 BATTERY = {
-    "url": "http://hostname/cgi/ems_data.js",
+    "url": "http://hostname/cgi/ems_data.xml",
     "name": "TheBattery"
 }
 
@@ -13,91 +13,22 @@ INFLUX = {
 }
 
 response = requests.get(BATTERY['url'])
+root = ET.fromstring(response.text)
+print(f"response: {response.text}")
 
 # State of Charge
-soc = """
-var sum = 0;
-var chargerCount = 2;
-
-for (var i = 0; i < chargerCount; i++) {
-    if (typeof Charger_Data[i] == "undefined") {
-      continue;
-    }
-
-    sum += Charger_Data[i][2]; 
-}
-Math.round(sum/chargerCount);
-"""
-
-ems_data = response.text
-battery_percent_full = js2py.eval_js(ems_data + soc)
+battery_percent_full = root.find(".//var[@name='SOC']").attrib["value"]
 print(f"procent full: {battery_percent_full}")
 
-gridpower = """
-WR_U_VERBUND_L1_INDEX = 5;
-WR_U_VERBUND_L2_INDEX = 6;
-WR_U_VERBUND_L3_INDEX = 7;
-WR_I_VERBUND_L1_INDEX = 8;
-WR_I_VERBUND_L2_INDEX = 9;
-WR_I_VERBUND_L3_INDEX = 10;
-Math.round((
-        WR_Data[WR_U_VERBUND_L1_INDEX] * WR_Data[WR_I_VERBUND_L1_INDEX]
-      + WR_Data[WR_U_VERBUND_L2_INDEX] * WR_Data[WR_I_VERBUND_L2_INDEX]
-      + WR_Data[WR_U_VERBUND_L3_INDEX] * WR_Data[WR_I_VERBUND_L3_INDEX]
-      ) / 100);
-"""
+# State
+battery_state = root.find(".//var[@name='State']").attrib["value"]
+print(f"State: {battery_state}")
 
-power_grid = js2py.eval_js(ems_data + gridpower)
-print(f"watts from/to grid: {power_grid}")
 
-batteriepower = """
-WR_U_INSEL_L1_INDEX = 11;
-WR_U_INSEL_L2_INDEX = 12;
-WR_U_INSEL_L3_INDEX = 13;
-WR_I_INSEL_L1_INDEX = 14;
-WR_I_INSEL_L2_INDEX = 15;
-WR_I_INSEL_L3_INDEX = 16;
-Math.round((
-        WR_Data[WR_U_INSEL_L1_INDEX] * WR_Data[WR_I_INSEL_L1_INDEX]
-      + WR_Data[WR_U_INSEL_L2_INDEX] * WR_Data[WR_I_INSEL_L2_INDEX]
-      + WR_Data[WR_U_INSEL_L3_INDEX] * WR_Data[WR_I_INSEL_L3_INDEX]
-      ) / 100);
-"""
-
-power_batterie = js2py.eval_js(ems_data + batteriepower)
-print(f"watts from/to battrie: {power_batterie}")
-
-productionpower = """
-WR_PMB_INDEX = 39;
-Math.round(WR_Data[WR_PMB_INDEX])
-"""
-
-power_production = js2py.eval_js(ems_data + productionpower)
-print(f"watts production: {power_production}")
-
-def calulate_power_usage():
-    if power_production <= 0:
-        if power_grid <= 0 and power_batterie <= 0:
-            return abs(power_grid + power_batterie)
-        else:
-            return 0
-    else:
-        if grid > 0:
-            return power_production - power_grid - power_batterie
-        else:
-            return power_production - power_batterie + abs(power_grid)
-
-# calculate power usage
-power_usage = calulate_power_usage()
-print(f"watts usage: {power_usage}")
-
-data = "battery,name={} percentfull={}i,grid={}i,battery={}i,production={}i,usage={}i".format(
+data = "battery,name={} percentfull={}i,state={}i".format(
     BATTERY['name'],
     battery_percent_full,
-    power_grid,
-    power_batterie,
-    power_production,
-    power_usage
+    battery_state
 )
 
 r = requests.post(INFLUX["url"], data=data, auth=(INFLUX["username"], INFLUX["password"]))
